e7531c7822ea0085645c26b90d3b4864f9e00775
Kazuya Takeshima <mail@mitukiii.jp>
2011-07-01 13:57:46 +0900

    Push when status is star or id_call.

--- app/app.rb
+++ app/app.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 class Hatebu < Padrino::Application
   register ScssInitializer
   register Padrino::Mailer
@@ -5,7 +6,27 @@ class Hatebu < Padrino::Application
 
   enable :sessions
 
+  def boxcar
+    @boxcar ||= Boxcar.find :first
+  end
+
+  def boxcar_api
+    @boxcar_api ||= BoxcarAPI::User.new boxcar.email, boxcar.password
+  end
+
   post :index do
     @hook = Hook.create params
+    case @hook.status
+    when 'star'
+      message = "#{@hook.title} #{@hook.quote}"
+      title = "id:#{@hook.username}: #{@hook.color} star"
+      source = @hook.permalink
+      boxcar_api.notify message, title, nil, source
+    when 'id_call'
+      message = "#{@hook.titke} #{@hook.comment}"
+      title = "id:#{@hook.username}: id call"
+      source = @hook.permalink
+      boxcar_api.notify message, title, nil, source
+    end
   end
 end
